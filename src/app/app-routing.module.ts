2b5adc35087d83bb5a44727b24d2ae94fb517d0f
Tecla Roig <ch.roig@gmail.com>
2021-08-09 19:29:54 -0300

    pagina de status y useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StatusComponent } from './shared/pages/status/status.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'status',
+    component: StatusComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusComponent } from './shared/pages/status/status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
